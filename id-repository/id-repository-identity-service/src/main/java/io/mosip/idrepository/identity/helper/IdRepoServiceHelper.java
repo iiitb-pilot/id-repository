8d9df004eca52fc8a5407e8f86fe7b2bef33dfd6
Muralitharan K <muralitharan@mosip.io>
2024-05-20 14:32:48 +0530

    Converts string handle value into array

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/IdRepoServiceHelper.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/helper/IdRepoServiceHelper.java
@@ -6,7 +6,6 @@ import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
 import com.jayway.jsonpath.PathNotFoundException;
-
 import io.mosip.idrepository.core.builder.RestRequestBuilder;
 import io.mosip.idrepository.core.constant.IdRepoErrorConstants;
 import io.mosip.idrepository.core.constant.RestServicesConstants;
@@ -153,7 +152,13 @@ public class IdRepoServiceHelper {
             if(identityMap.containsKey(selectedHandlesFieldId) && Objects.nonNull(identityMap.get(selectedHandlesFieldId))) {
                 mosipLogger.debug(IdRepoSecurityManager.getUser(), ID_REPO_SERVICE_HELPER, "getSelectedHandles",
                         requestMap.get(selectedHandlesFieldId));
-                List<String> selectedHandleFieldIds = (List<String>) identityMap.get(selectedHandlesFieldId);
+                List<String> selectedHandleFieldIds = null;
+                if (identityMap.get(selectedHandlesFieldId) instanceof String) {
+                    selectedHandleFieldIds = Arrays.asList(((String) identityMap.get(selectedHandlesFieldId)).split(","));
+                } else {
+                    selectedHandleFieldIds = (List<String>) identityMap.get(selectedHandlesFieldId);
+                }
+
                 return selectedHandleFieldIds.stream()
                         .filter( handleFieldId -> supportedHandlesInSchema.get(schemaVersion).contains(handleFieldId))
                         .collect(Collectors.toMap(handleName->handleName,
